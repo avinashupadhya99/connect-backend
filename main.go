e445c78801bedc04ac04bb9069d059d2fe247251
K R Avinash Upadhyaya <kupadhyaya@deloitte.com>
2021-10-09 01:06:13 +0530

    feat: get slot details

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func initializeRouter() {
 	r.HandleFunc("/api/users/{id}", GetUser).Methods("GET")
 	r.HandleFunc("/api/users", CreateUser).Methods("POST")
 	r.HandleFunc("/api/slot/book", BookSlot).Methods("POST")
+	r.HandleFunc("/api/slot", GetSlot).Methods("GET")
 	log.Fatal(http.ListenAndServe(":9000", r))
 }
 
--- slot.go
+++ slot.go
@@ -29,3 +29,14 @@ func BookSlot(w http.ResponseWriter, r *http.Request) {
 	DB.Save(&slot)
 	json.NewEncoder(w).Encode(slot)
 }
+
+func GetSlot(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var slot Slot
+	starttime := r.URL.Query().Get("starttime")
+	endtime := r.URL.Query().Get("endtime")
+	date := r.URL.Query().Get("date")
+
+	DB.Preload("Users").First(&Slot{StartTime: starttime, EndTime: endtime, Date: date}).Find(&slot)
+	json.NewEncoder(w).Encode(slot)
+}

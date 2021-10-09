5369d60630b4bfe7fa4e3d00d779a7aa72839edc
K R Avinash Upadhyaya <kupadhyaya@deloitte.com>
2021-10-09 18:42:56 +0530

    fix: use date type for date in slot

--- slot.go
+++ slot.go
@@ -2,32 +2,49 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
+	"gorm.io/datatypes"
 	"gorm.io/gorm"
 )
 
 type Slot struct {
 	gorm.Model
-	StartTime string  `json:"starttime" gorm:"primaryKey"`
-	EndTime   string  `json:"endtime" gorm:"primaryKey"`
-	Date      string  `json:"date" gorm:"primaryKey"`
-	Users     []*User `json:"users" gorm:"many2many:slot_users;"`
-	UserID    int     `json:"userid" gorm:"-"`
+	StartTime string         `json:"starttime" gorm:"primaryKey"`
+	EndTime   string         `json:"endtime" gorm:"primaryKey"`
+	Date      datatypes.Date `json:"-" gorm:"primaryKey"`
+	DateStr   string         `json:"date" gorm:"-"`
+	Users     []*User        `json:"users" gorm:"many2many:slot_users;"`
+	UserID    int            `json:"userid" gorm:"-"`
 }
 
 func BookSlot(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var slot Slot
 	json.NewDecoder(r.Body).Decode(&slot)
-	DB.FirstOrInit(&slot)
-	var user User
-	DB.First(&user, slot.UserID)
-	if !userInSlice(user, slot.Users) {
-		slot.Users = append(slot.Users, &user)
+	date, err := time.Parse("2006-01-02", slot.DateStr)
+	fmt.Println("date", date)
+	if err == nil {
+		slot.Date = datatypes.Date(date)
+		DB.FirstOrInit(&slot)
+		var user User
+		DB.First(&user, slot.UserID)
+		fmt.Println(user)
+
+		if !userInSlice(user, slot.Users) {
+			slot.Users = append(slot.Users, &user)
+		}
+		DB.Save(&slot)
+		json.NewEncoder(w).Encode(slot)
+		return
+	} else {
+		log.Println(err.Error())
+		http.Error(w, "Invalid date format, should be YYYY-MM-DD", http.StatusBadRequest)
+		return
 	}
-	DB.Save(&slot)
-	json.NewEncoder(w).Encode(slot)
 }
 
 func GetSlot(w http.ResponseWriter, r *http.Request) {
@@ -35,8 +52,14 @@ func GetSlot(w http.ResponseWriter, r *http.Request) {
 	var slot Slot
 	starttime := r.URL.Query().Get("starttime")
 	endtime := r.URL.Query().Get("endtime")
-	date := r.URL.Query().Get("date")
+	date, err := time.Parse("2006-01-02", r.URL.Query().Get("date"))
+	fmt.Println("date", date)
 
-	DB.Preload("Users").First(&Slot{StartTime: starttime, EndTime: endtime, Date: date}).Find(&slot)
-	json.NewEncoder(w).Encode(slot)
+	if err == nil {
+		DB.Preload("Users").Where(&Slot{StartTime: starttime, EndTime: endtime, Date: datatypes.Date(date)}).First(&slot)
+		json.NewEncoder(w).Encode(slot)
+	} else {
+		log.Println(err.Error())
+		http.Error(w, "Invalid date format, should be YYYY-MM-DD", http.StatusBadRequest)
+	}
 }

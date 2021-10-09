6f2c01f980cc3371faf91ba8ddc47e7af5b8a6ab
K R Avinash Upadhyaya <kupadhyaya@deloitte.com>
2021-10-09 13:31:48 +0530

    build: deploy to google cloud app engine

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -14,7 +15,13 @@ func initializeRouter() {
 	r.HandleFunc("/api/users", CreateUser).Methods("POST")
 	r.HandleFunc("/api/slot/book", BookSlot).Methods("POST")
 	r.HandleFunc("/api/slot", GetSlot).Methods("GET")
-	log.Fatal(http.ListenAndServe(":9000", r))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+		log.Printf("Defaulting to port %s", port)
+	}
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
 func main() {
